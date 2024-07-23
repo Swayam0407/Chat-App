3b47b7d42bb5ef3fae5494f9123850cc5b01aa97
Swayam <125536817+Swayam0407@users.noreply.github.com>
2024-07-23 11:47:28 +0500

    Add files via upload

---
+++ chatserver/src/main/java/com/involveininovation/chatserver/model/Message.java
@@ -0,0 +1,19 @@
+package com.involveininovation.chatserver.model;
+
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+
+}
---
+++ chatserver/src/main/java/com/involveininovation/chatserver/model/Status.java
@@ -0,0 +1,7 @@
+package com.involveininovation.chatserver.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
